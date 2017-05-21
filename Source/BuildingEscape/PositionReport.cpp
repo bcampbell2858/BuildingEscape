cce3ed8e717ed6c9b4f106983d616784a01a6493
bcampbell2858 <bcampbell2858@hotmail.com>
2017-05-21 17:32:32 -0400

    Good Commit

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -10,8 +10,7 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+		
 }
 
 
@@ -19,8 +18,8 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s"), *ObjectName);
 	
 }
 
